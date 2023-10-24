8989170961d5bec01f41664c0ca36ffb690df5ee
Mustafa Buğra Kayim <mbugrakayim@gmail.com>
2023-10-24 13:28:36 +0300

    Odev

---
+++ homework-class-2/src/App.js
@@ -0,0 +1,22 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import ImageMap from "./Components/ImageMap";
+import Navbar from "./Components/Navbar";
+import PageContent from "./Components/PageContent/PageContent";
+
+function App() {
+  return (
+
+    <>
+      <Navbar></Navbar>
+      <Header />
+      <PageContent />
+      <ImageMap />
+      <Footer />
+    </>
+
+
+  );
+}
+
+export default App;
